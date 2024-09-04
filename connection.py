546477f5528f991d272f99a4849adf31a00a54fb
Tim Gresham <tjgresha@gmail.com>
2024-09-04 10:38:13 -0700

    Update connection.py

--- connection.py
+++ connection.py
@@ -1,3 +1,13 @@
 import openstack
 from openstack.config import loader
 
+
+openstack.enable_logging(True, stream=sys.stdout)
+
+
+class Opts:
+    def __init__(self, cloud_name='openstack', debug=False):
+        self.cloud = cloud_name
+        self.debug = debug
+        # Use identity v3 API for examples.
+        self.identity_api_version = '3'
